2db339adf2795e5a58a3fd1251ed318166c88fba
Tendai <tendai1@hotmail.com>
2021-09-27 00:25:06 +0200

    Set the auths for facebook and google providers

--- src/components/Login.js
+++ src/components/Login.js
@@ -9,12 +9,18 @@ const Login = () => {
         <div id="login-page">
           <div id="login-card">
             <h2>Welcome to RadicalChat</h2>
-            <div className="login-button google">
+            <div 
+            className="login-button google"
+            onClick={() => auth.signInWithRedirect(new firebase.auth.GoogleAuthProvider())}
+            >
 
               <GoogleOutlined /> Sign In with Google
             </div>
             <br /> <br /> 
-            <div className="login-button facebook">
+            <div 
+            className="login-button facebook"
+              onClick={() => auth.signInWithRedirect(new firebase.auth.FacebookAuthProvider())}
+            >
 
               <FacebookOutlined /> Sign In with Facebook
             </div>
