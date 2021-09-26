60088e44d911fdae679969a6696d9e25e521a710
Tendai <tendai1@hotmail.com>
2021-09-26 23:31:38 +0200

    Add the login card

--- src/components/Login.js
+++ src/components/Login.js
@@ -4,7 +4,9 @@ import { GoogleOutlined, FacebookOutlined } from '@ant-design/icons';
 const Login = () => {
       return (
         <div id="login-page">
-          
+          <div id="login-card">
+            <h2>Welcome to RadicalChat</h2>
+          </div>
         </div>
       )
 }
